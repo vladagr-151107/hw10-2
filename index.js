038e8a29dc4adbac279fccad50f5aa108cdb6bcb
Vlada Grushchenko <vladagrushchenko@gmail.com>
2025-07-07 15:32:56 +0300

    hw10-2

---
+++ index.js
@@ -0,0 +1,16 @@
+const numbers = [1, 4, 5, 88, 33, 34, 45, 1020];
+function sortArray(){
+    let newArray = [];
+    for(i = 0; i < numbers.length; i++){
+        if(numbers[i] % 2 === 0){
+            newArray.push(numbers[i]);
+        }
+    }
+    return newArray;
+}
+console.log(sortArray());
+// або 
+const evenNumbers = numbers.filter((num) => {
+    return num % 2 === 0;
+});
+console.log(evenNumbers);
